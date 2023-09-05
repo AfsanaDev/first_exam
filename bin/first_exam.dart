027ea837d27dddef143f5f3d4432328087eddcf3
Afsana Akter <tarinakter567@gmail.com>
2023-09-05 18:24:06 +0600

    Added some variables in constructor.

--- bin/first_exam.dart
+++ bin/first_exam.dart
@@ -1,33 +1,62 @@
 import 'dart:ffi';
-
 import 'package:first_exam/first_exam.dart' as first_exam;
 
 
 class Person{
   String _firstName;
   String _lastName;
-  Person(this._firstName, this._lastName);
+  Person({ String firstName =" ", String lastName = ""}): _firstName = firstName, _lastName= lastName;
 
-  void show( String firstName, String lastName){
-    _firstName = firstName;
-    _lastName = lastName;
-   print("Name: $firstName $lastName");
+  String get getFullName{
+    return  "${this._firstName} ${this._lastName}";
+  }
 
+  void show(){
+
+   print("Name: ${this._firstName} ${this._lastName}");
 
   }
+
+
 
 }
 
 class Teacher extends Person {
   int _noOfPublications =0 ;
 
-  Teacher(super.firstName, super.lastName );
+  Teacher({ String firstName =" ", String lastName = "", int noOfpublication=0}):_noOfPublications = noOfpublication,
+  super(firstName: firstName, lastName: lastName);
+  @override
+  void show( ){
+    print("Name: ${super.getFullName} , No of Publication : $_noOfPublications");
+
+  }
 
 
 }
+
+class Student extends Person{
+  double _cgpa ;
+  Student({ String firstName =" ", String lastName = "", double cgpa =0.0}):_cgpa = cgpa,
+        super(firstName: firstName, lastName: lastName);
+  @override
+  void show( ){
+    print("Name: ${super.getFullName} , CGPA : $_cgpa");
+
+  }
+}
 void main(){
-  // Teacher teacher = Teacher("Afsana", "Akter");
-  // teacher.show("firstName", "lastName");
-  Person person = Person("Afsana", "Akter");
-  person.show("firstName", "lastName");
+
+  List<Person> personList =[];
+  personList.add(Teacher(firstName:"Afsan", lastName: "akter", noOfpublication: 2));
+  personList.add(Student(firstName:"Afsan", lastName: "akter" , cgpa: 3.80));
+  personList.add(Student(firstName:"akhi", lastName: "akter" , cgpa: 3.0));
+  personList.add(Teacher(firstName:"Afsan", lastName: "akter", noOfpublication: 2));
+  personList.add(Teacher(firstName:"Afsan", lastName: "akter", noOfpublication: 2));
+
+  for(Person  person in personList){
+    person.show();
+  }
+
+
 }
