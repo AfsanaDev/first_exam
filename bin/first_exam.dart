566124d9fdc4e4bb40975fbc90aa2b70a847e78b
Afsana Akter <tarinakter567@gmail.com>
2023-09-05 18:24:06 +0600

    Added some variables in constructor.

--- bin/first_exam.dart
+++ bin/first_exam.dart
@@ -6,11 +6,11 @@ import 'package:first_exam/first_exam.dart' as first_exam;
 class Person{
   String _firstName;
   String _lastName;
-  Person(this._firstName, this._lastName);
+  Person({ String firstName =" ", String lastName = ""}): _firstName = firstName, _lastName= lastName;
 
   void show( String firstName, String lastName){
-    _firstName = firstName;
-    _lastName = lastName;
+    this._firstName = firstName;
+    this._lastName = lastName;
    print("Name: $firstName $lastName");
 
 
@@ -21,13 +21,19 @@ class Person{
 class Teacher extends Person {
   int _noOfPublications =0 ;
 
-  Teacher(super.firstName, super.lastName );
+  Teacher({ String firstName =" ", String lastName = "", int noOfpublication=0}):_noOfPublications = noOfpublication,
+  super(firstName: firstName, lastName: lastName);
+
+  int get onOfpublication=> _noOfPublications;
+  void show(){
+
+  }
 
 
 }
 void main(){
   // Teacher teacher = Teacher("Afsana", "Akter");
   // teacher.show("firstName", "lastName");
-  Person person = Person("Afsana", "Akter");
-  person.show("firstName", "lastName");
+  Person person = Person();
+  person.show("Afsana", "Akter");
 }
