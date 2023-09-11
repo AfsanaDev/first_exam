1687466b2dc40892c67aebc7a79fcc59eff590f0
Afsana Akter <tarinakter567@gmail.com>
2023-09-11 10:49:40 +0600

    All classes and functions are initialite now.

--- bin/duck.dart
+++ bin/duck.dart
@@ -1,19 +1,60 @@
 class Duck{
   void quack(){
-    print("All types of duck can quack ");
+    print("Duck: All types of duck can quack ");
   }
   void swim(){
-    print("All duck can swim by birth");
+    print("Duck: All duck can swim by birth");
   }
+
+  void display(){
+    print("Duck: All types duck can displaying");
+  }
+
   void fly(){
-    print("All duck can fly");
+    print("Duck: All duck can fly");
   }
 }
 
 class MallarDuck extends Duck{
+  @override
+  void display(){
+    print("MallarDuck: Mallarduck can displaying");
+  }
+  @override
+  void fly(){
+    print("MallarDuck: Mallarduck also can fly");
+  }
 
 }
 
 class RedHeadDuck extends Duck{
 
+  @override
+  void display(){
+    print("ReadHeadDuck: Mallarduck can displaying");
+  }
+  @override
+  void fly(){
+    print("RedHeadDuck: Mallarduck also can fly");
+  }
+}
+
+void main(){
+  Duck duck = Duck();
+  duck.quack();
+  duck.swim();
+  duck.display();
+  duck.fly();
+
+  MallarDuck mallarDuck = MallarDuck();
+  mallarDuck.quack();
+  mallarDuck.swim();
+  mallarDuck.display();
+  mallarDuck.fly();
+
+  RedHeadDuck redHeadDuck = RedHeadDuck();
+  redHeadDuck.quack();
+  redHeadDuck.swim();
+  redHeadDuck.display();
+  redHeadDuck.fly();
 }
