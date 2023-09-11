c9da94942b8fcc54d6921fab6bb9be4bb5bc17b0
Afsana Akter <tarinakter567@gmail.com>
2023-09-11 10:35:47 +0600

    Create some class and also functions and initialite them

---
+++ bin/duck.dart
@@ -0,0 +1,19 @@
+class Duck{
+  void quack(){
+    print("All types of duck can quack ");
+  }
+  void swim(){
+    print("All duck can swim by birth");
+  }
+  void fly(){
+    print("All duck can fly");
+  }
+}
+
+class MallarDuck extends Duck{
+
+}
+
+class RedHeadDuck extends Duck{
+
+}
